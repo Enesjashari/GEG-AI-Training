482710cc338da7097b32158f4a1f0e97a5a61371
Enes <enesjashari2004@gmail.com>
2026-02-14 18:51:47 +0100

    Kodi

---
+++ M1 - L3/main.py
@@ -0,0 +1,107 @@
+
+
+# Loops -> Nje koncept ne Python qe perdoret me perserit kodin 
+
+# Na nevoitet nje loop ne nje skript e cila mund te merr shume votues 
+
+
+
+
+
+# To many incrrorect times. Try Again!   == Cooldown 
+ 
+
+# Ne Python i kemi 2 loop 
+
+# While 
+
+# For loop
+
+#Do While -> Ne gjuhet tjera eshte ne Python Jo 
+
+# import time
+
+
+# number = 0
+
+# while number <= 10: 
+#     print(number)
+#     # Increment
+#     number = number + 1
+#     time.sleep(1)
+
+
+# Te printohen numrat nga 0 - 20
+# Te printohen numrat nga -10 - +10
+
+
+# # Home -> Kthen artikujt e lajmeve 
+# def home():
+
+
+# number = 0
+# while number <= 20:
+#     print(number)
+#     number = number + 1
+
+# number = -10
+# while number <=10: 
+#     print(number)
+#     number = number + 1
+
+
+
+# number1 = -10 
+# while number1 <= 10:
+#     print(number1)
+#     number1 = number1 + 1
+#     number2 = 0
+
+# while number2 <= 20:
+#     print(number2) 
+#     number2 = number2 +1
+ 
+# number = 0
+# while number <= 20:
+#     print(number)
+#     number= number + 1
+ 
+ 
+# number = -10
+# while number <=10:
+#     print(number)
+#     number = number + 1
+
+
+# start = numri fillestart
+# end = numri i fundit - 1
+# step = sa numra deshiron me i kalu
+
+# range(start, end, step)
+# for i in range()
+
+for i in range(1, 21 ):
+    print(i)
+
+
+# Duke perdor For Loop te printohet numrat nga 15 deri ne 100 duke u perfshire edhe keta dy numra
+
+
+# Duke perdor range nga 2 deri ne 16 , per secilin numer te tregohet nese eshte qift apo tek 
+#p.sh 
+
+# 2 qift
+# 3 tek
+# 4 qift
+# 5 tek
+
+
+for i in range(15, 101):
+    print(i)
+ 
+for i in range(2, 17):
+    if i % 2 == 0:
+        print(str(i) + " cift")  
+    else:
+        print(str(i) + " tek")   
+ 
