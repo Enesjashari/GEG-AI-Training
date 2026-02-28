e7d448401a391eff5ae6a08bf248b12b028ddb0b
Enes <enesjashari2004@gmail.com>
2026-02-28 17:16:51 +0100

    Lecture Push

---
+++ M2 - L2/main.py
@@ -0,0 +1,39 @@
+
+
+
+# Cka eshte nje array ? 
+# Eshte nje grumbull i te dhenave / elementeve
+
+
+# Cilat jane disa prej karkteristikave qe dellojn List & Numpy Array.
+'''
+
+•Listat mundemi me pas tipe te llojeve te ndryshme brenda 
+
+•Array lejojn vetem nje tip
+
+array 70x faster than list 
+
+'''
+
+
+# import numpy as np
+
+# arr = np.array([10, 20, 30])
+
+# # Create (add)
+# arr = np.append(arr, 40)     # [10 20 30 40]
+
+# # Read
+# print(arr[1])                # 20
+
+# # Update
+# arr[2] = 99                  # [10 20 99 40]
+
+# # Delete
+# arr = np.delete(arr, 0)      # [20 99 40]
+
+# print(arr)
+
+
+
---
+++ M2 - L2/new.py
@@ -0,0 +1,129 @@
+
+# CRUD me list,array x
+# Dimenzionet
+#Shape
+#Reshaping
+# Join
+# Search
+#Filter
+#Sort
+
+
+#Ex1  Array1
+
+#----------------------------------
+#Tuple, dictionary , set
+
+
+import numpy as np 
+
+#                   0   1   2    3  4
+array1 = np.array([100,200,300,400,500])
+
+
+# print(array1[2])
+
+
+#lista
+list1 = [100,200,300,400,500]
+
+#CRUD ne list 
+#append() -> Shton nje element ne fund te listes
+#insert() -> Shton nje element ne baze te indexit
+
+
+print("Para: ",list1)
+
+list1.append(600)
+list1.append('apple')
+
+
+# list1.
+list1.insert(4,'orange')
+list1.insert(0,"kiwi")
+
+
+
+
+#Read
+
+print(list1[0])
+print(list1[5])
+print(list1[-1])
+# print(list1)
+
+#Update
+#Syntax  list_name[index] = new_value
+
+list1[2] = "Kosova"
+list1[-1] = "Austria"
+
+list1[-4] = "mango"
+
+
+#delete dhe remove
+del list1[0]
+del list1[-3]
+
+list1.remove("mango")
+list1.remove("Austria")
+
+print("Pas: ",list1)
+
+
+#Te krijohet nje list me 5 elemente qfardo x
+#te shtohet nje element ne fund dhe nje ne indexin 4
+#te behen update 2 elementet e para 
+#te fshihen 2 elementet e para
+#printo te gjithe listen
+
+'''# lista me 5 elemente
+lista = [10, 20, 30, 40, 50]
+print(lista)
+ 
+# Te shtohet nje element ne fund dhe nje me indeksin 4
+lista.append(60)
+lista.insert(4, 45)
+print("Pas shtimit:", lista)
+ 
+# Te behen update 2 elementet e para
+lista[0] = 15
+lista[1] = 25
+print("Pas update:", lista)
+ 
+# Te fshihen dy elementet e para
+del lista[0]
+lista.remove(25)
+ 
+# Printo listen perfundimtare
+print("Lista perfundimtare:", lista)
+'''
+
+
+
+print('------------------------------')
+
+
+array3 = np.array([1000 , 2000 , 3000 , 4000])
+
+
+#CRUD
+#Create
+print("Para: ", array3)
+new_array = np.append(array3 , 5000)
+# print("Pas: ", new_array)
+
+
+#Read
+print(new_array[4])
+print(new_array[0])
+
+#Update
+new_array[0] = 5
+new_array[1] = 2
+
+#delete
+new_array  = np.delete(new_array ,2 )
+new_array  = np.delete(new_array ,3 )
+
+print("Pas: ", new_array)
